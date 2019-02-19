ae48871d0543de3334daf91cc1699183b04e73fb
joao <joao182cx@gmail.com>
2019-02-19 16:40:12 -0300

    Criação do código que exibe as palavras que são possíveis de serem formadas com a palavra inserida pelo usuário.

--- src/TresLetras.java
+++ src/TresLetras.java
@@ -6,7 +6,9 @@ public class TresLetras {
 		
 		String palavra;
 		String[] letras;
-		String[] palavrasTres;
+		String[] palavrasTres = new String[60];
+		int cont = 0;
+		
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -15,9 +17,43 @@ public class TresLetras {
 		
 		letras = palavra.split("");
 		
-		palavrasTres = new String[(int)Math.pow(3.0, (double)letras.length)];
+		//palavrasTres = new String[(int)Math.pow(3.0, (double)letras.length)];
+	
+		for(int i = 0; i < letras.length; i++) {
+			
+			//System.out.printf("%d", i);
+			
+			for(int j = 0; j < letras.length; j++) {
+				
+				if(j != i) {
+					
+					for(int k = 0; k < letras.length; k++) {
+						
+						if(k != i && k != j) {
+							
+							palavrasTres[cont] = String.format("%s%s%s", letras[i], letras[j], letras[k]);
+							cont++;
+							
+						}
+						
+					}
+					
+				}
+				
+				
+				
+			}
+			
+		}
 		
-		System.out.println(palavrasTres.length);
+		System.out.printf("Palavras possíveis com as letras \"%s\".%n%n", palavra);
+		
+		for(String a : palavrasTres) {
+			
+			System.out.println(a);
+			
+		}
+		
 	}
 
 }
